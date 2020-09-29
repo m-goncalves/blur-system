9f584368bd3a412ddbf9ab5faf6c409c7e862f36
Marcelo Goncalves <mgoncalves@anynines.com>
2020-09-29 19:10:13 -0300

    argument parser working

--- transformation/blur.py
+++ transformation/blur.py
@@ -1,10 +1,14 @@
 from cvlib import detect_face
 from cv2 import GaussianBlur, imread, imwrite, imshow, waitKey, destroyAllWindows
-#import cvlib as cv
+from argparse import ArgumentParser, ArgumentTypeError
 import os
 
 # readind input image
-image = imread('/Users/marcelo/Desktop/imgs/marcelo.jpg')
+parser = ArgumentParser(description="Blurring faces of images")
+parser.add_argument('source_path', metavar="source", type=str, help="Path from the image to be blurred")
+parser.add_argument('destination_path', metavar="destination", type=str, help="Path where the image will be saved")
+args = parser.parse_args()
+image = imread(args.source_path)
 
 # applying face detection. returns the coordinates
 faces, _  = detect_face(image)
@@ -14,15 +18,15 @@ for face in faces:
 
     #assigning the coordinates to the variables
     startX, startY, endX, endY = face
-    #building point to be blurried
+    #building point to be blurred
     face_image = image[startY:endY, startX:endX]
     #blurring faces
     face_image = GaussianBlur(face_image, (23, 23), 30)
-    #rebuilding the image. "shape[0] and height returns nr. of rows and columns"
+    #rebuilding the image. "shape[0] and height returns nr. of rows and nr. of columns"
     # dont understand the logics
     width = face_image.shape[0]
     height = face_image.shape[1]
     image[startY:startY+width, startX:startX+height] = face_image   
 
-imshow("face_detection", image)
-waitKey()
+imwrite(args.destination_path, image)
+# '/Users/marcelo/Desktop/imgs/marcelo.jpg'
