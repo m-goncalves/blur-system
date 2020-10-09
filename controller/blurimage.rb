7398861b78c8bbeb28a55d948e772863120825bc
Marcelo Goncalves <mgoncalves@anynines.com>
2020-10-09 12:36:07 -0300

    fixing worker bug

--- controller/blurimage.rb
+++ controller/blurimage.rb
@@ -1,11 +1,20 @@
 args = ARGV
 
+sourceFile = args[0].dup
+
 destinationDir = "blurred_images"
+
 system("mkdir -p " + destinationDir)
-# getting the index of the substring "/" from the array
-idx = args[0].index("/")
+
+if sourceFile.include? "/"
+    idx = sourceFile.length - sourceFile.reverse!.index("/")
+    sourceFile.reverse!   
+else
+    idx = 0
+end
+
 # assigning to the variable "destination" the string "blurred_images", the character "/" and everything that comes after it
-destination = destinationDir + "/" + args[0][idx+1..-1]
-# calling the python programm and passing the arguments it is able to receive: the image to be blurred and where it has to be placed
-system("python transformation/blur.py " + args[0] + " " + destination)
-system("rm -f " + args[0])
+destination = destinationDir + "/" + sourceFile[idx..-1]
+# calling the python programm and passing the arguments it needs: the image to be blurred and where it has to be placed
+system("python3 transformation/blur.py " + sourceFile + " " + destination)
+system("rm -f " + sourceFile)
