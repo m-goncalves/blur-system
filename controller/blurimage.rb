8553e09dc2dc57c4be681627f88465cf030bafac
Marcelo Goncalves <mgoncalves@anynines.com>
2020-10-23 21:48:52 -0300

    removing unnecessary line and commenting rabbitmq functions

--- controller/blurimage.rb
+++ controller/blurimage.rb
@@ -17,17 +17,22 @@ def destinationPath(sourcePath)
 end
 
 # RabbitMQ initialization
-
+# Defining the connection to the specific host (defined in the go code after "@")
 connection = Bunny.new(host: "rabbitmq")
+# Starting the connection
 connection.start
-channel = connection.create_channel  
+# Creating the channel to receive messages
+channel = connection.create_channel 
+# Specifying the desired queue
 queue = channel.queue('blur-service')
 sourceFile = ""
 
 begin
+    # Receives data from rabbitmq when it hat new messages and defines what has to be done
     queue.subscribe(block: true) do |  _delivery_info, _properties, filepath |
         # calling the python programm and passing the arguments it needs: the image to be blurred and where it has to be placed
         output = system "python3", "transformation/blur.py", filepath, destinationPath(filepath)
+        # Removing the original
         system("rm -f " + filepath)
     end
 rescue
@@ -35,5 +40,4 @@ rescue
 ensure
     connection.close()
     #removing original file
-    system("rm -f " + sourceFile)
 end
