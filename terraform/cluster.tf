096fef9853076a1445e329f03704ffcd6328177f
Marcelo Goncalves <tchelojg@gmail.com>
2020-12-20 20:02:13 -0300

    adding worker nodes

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -82,7 +82,7 @@ resource "aws_iam_role_policy_attachment" "blur-iam-role-AmazonEKSClusterPolicy"
 
 resource "aws_security_group" "blur-cluster" {
     name            = "eks-blur-cluster"
-    description     = "Allows the communucation with worker nodes"
+    description     = "Allows the communucation with the worker nodes"
     vpc_id          = "${aws_vpc.blur-vpc.id}"
 
     egress {
@@ -107,6 +107,7 @@ resource "aws_security_group" "blur-cluster" {
 #     type                    = "ingress"
 # }
 
+# Master node
 resource "aws_eks_cluster" "blur-cluster" {
     name = "${var.cluster-name}"
     role_arn = "${aws_iam_role.blur-iam-role.arn}"
@@ -120,4 +121,97 @@ resource "aws_eks_cluster" "blur-cluster" {
         "aws_iam_role_policy_attachment.blur-iam-role-AmazonEKSClusterPolicy",
         "aws_iam_role_policy_attachment.blur-iam-role-AmazonEKSClusterPolicy"
      ]
+}
+
+### worker nodes
+
+resource "aws_iam_role" "iam-role-worker"{
+    name = "eks-worker"
+    assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow"
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "iam-role-worker-AmazonEKSWorkerNodePolicy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+    role = "${aws_iam_role.iam-role-worker.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "iam-role-worker-AmazonEKS_CNI_Policy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+    role = "${aws_iam_role.iam-role-worker.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "iam-role-worker-AmazonEC2ContainerRegistryReadOnly" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+    role = "${aws_iam_role.iam-role-worker.name}"
+
+}
+
+resource "aws_iam_instance_profile" "worker-node" {
+    name = "worker-node"
+    role = "${aws_iam_role.iam-role-worker.name}"
+}
+
+# Security group for the worker nodes
+
+resource "aws_security_group" "security-group-worker" {
+    name = "worker-node"
+    description = "Security group for worker nodes"
+    vpc_id = "${aws_vpc.blur-vpc.id}"
+    egress = {
+        cidr_blocks = [ "0.0.0.0/0" ]
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+    }
+
+    tags = "${
+      map(
+          "Name", "blur-cluster",
+          "kubernetes.io/cluster/${var.cluster-name}", "owned"
+      )
+    }"
+}
+
+resource "aws_security_group_rule" "ingress-self" {
+    description = "Allow communication among nodes"
+    from_port = 0
+    to_port = 65535
+    protocol = "-1"
+    security_group_id = "${aws_security_group.security-group-worker.id}"
+    source_security_group_id = "${aws_security_group.security-group-worker.id}"
+    type = "ingress"
+}
+
+resource "aws_security_group_rule" "ingress-cluster-https" {
+    description = "Allow worker to receive communication from the cluster control plane"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    security_group_id = "${aws_security_group.security-group-worker.id}"
+    source_security_group_id = "${aws_security_group.blur-cluster.id}"
+    type = "ingress"
+    
+}
+
+resource "aws_security_group_rule" "ingress-cluster-others" {
+    description = "Allow worker to receive communication from the cluster control plane"
+    from_port = 1025
+    to_port = 65535
+    protocol = "tcp"
+    security_group_id = "${aws_security_group.security-group-worker.id}"
+    source_security_group_id = "${aws_security_group.blur-cluster.id}"
+    type = "ingress"
 }
