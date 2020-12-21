05058c49a79db01dccd32214283f2e7896b46f28
Marcelo Goncalves <tchelojg@gmail.com>
2020-12-21 17:28:31 -0300

    AWS worker nodes created.

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -17,7 +17,7 @@ resource "aws_vpc" "blur-vpc" {
     }"
 }
 
-resource "aws_subnet" "demo" {
+resource "aws_subnet" "subnet" {
   count = 2
 
   availability_zone = "${data.aws_availability_zones.available_zones.names[count.index]}"
@@ -51,7 +51,7 @@ resource "aws_route_table" "route-table" {
 
 resource "aws_route_table_association" "table_association" {
     count = 2
-    subnet_id       = "${aws_subnet.demo.*.id[count.index]}"
+    subnet_id       = "${aws_subnet.subnet.*.id[count.index]}"
     route_table_id  = "${aws_route_table.route-table.id}"
   
 }
@@ -114,7 +114,7 @@ resource "aws_eks_cluster" "blur-cluster" {
 
     vpc_config {
         security_group_ids  = ["${aws_security_group.blur-cluster.id}"]
-        subnet_ids          = "${aws_subnet.demo.*.id}"
+        subnet_ids          = "${aws_subnet.subnet.*.id}"
     }
 
     depends_on = [ 
@@ -132,7 +132,7 @@ resource "aws_iam_role" "iam-role-worker"{
     "Version": "2012-10-17",
     "Statement": [
         {
-            "Effect": "Allow"
+            "Effect": "Allow",
             "Principal": {
                 "Service": "ec2.amazonaws.com"
             },
@@ -170,7 +170,7 @@ resource "aws_security_group" "security-group-worker" {
     name = "worker-node"
     description = "Security group for worker nodes"
     vpc_id = "${aws_vpc.blur-vpc.id}"
-    egress = {
+    egress {
         cidr_blocks = [ "0.0.0.0/0" ]
         from_port = 0
         to_port = 0
@@ -214,4 +214,78 @@ resource "aws_security_group_rule" "ingress-cluster-others" {
     security_group_id = "${aws_security_group.security-group-worker.id}"
     source_security_group_id = "${aws_security_group.blur-cluster.id}"
     type = "ingress"
+}
+
+# Worker Access to Master
+
+resource "aws_security_group_rule" "cluster-node-ingress-http" {
+    description                     = "Allows pods to communicate with the cluster API server"
+    from_port                       = 443
+    to_port                         = "443"
+    protocol                        = "tcp"
+    security_group_id               = "${aws_security_group.blur-cluster.id}"
+    source_security_group_id        = "${aws_security_group.security-group-worker.id}"
+    type                            = "ingress"
+  
+}
+
+# Worker autoscaling group
+
+# This data will be used to filter and select an AMI which is compatible with the specific k8s version being deployed
+data "aws_ami" "eks-worker" {
+    filter {
+      name = "name"
+      values = ["amazon-eks-node-${aws_eks_cluster.blur-cluster.version}-v*"]
+    }
+
+    most_recent = true
+    owners = ["602401143452"] 
+}
+
+data "aws_region" "current" {}
+
+locals {
+    node-user-data =<<USERDATA
+#!/bin/bash
+set -o xtrace
+/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.blur-cluster.endpoint}'
+USERDATA
+}
+
+resource "aws_launch_configuration" "launch_config" {
+    associate_public_ip_address     = true
+    iam_instance_profile        = "${aws_iam_instance_profile.worker-node.name}"
+    image_id                    = "${data.aws_ami.eks-worker.id}"
+    instance_type               = "t2.micro"
+    name_prefix                 = "terraform-eks"
+    security_groups             = ["${aws_security_group.security-group-worker.id}"]
+    user_data_base64            = "${base64encode(local.node-user-data)}"
+    lifecycle {
+      create_before_destroy     = true
+    }
+  
+}
+
+# Autoscaling group
+
+resource "aws_autoscaling_group" "autoscaling" {
+    desired_capacity = 2
+    launch_configuration        = "${aws_launch_configuration.launch_config.id}" 
+    max_size                    = 2
+    min_size                    = 1
+    name                        = "terraform-eks"
+    vpc_zone_identifier         = "${aws_subnet.subnet.*.id}"
+
+    tag {
+      key = "Name"
+      value = "terraform-eks"
+      propagate_at_launch = true
+    }
+
+# "kubernetes.io/cluster/*" tag allows EKS and K8S to discover and manage compute resources.
+    tag {
+      key                       = "kubernetes.io/cluster/${var.cluster-name}"
+      value                     = "owned"
+      propagate_at_launch       = true
+    }
 }
