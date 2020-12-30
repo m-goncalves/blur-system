44c46f5a9e7875be44ec8eb1e137131d49b682a1
marcelo goncalves <tchelojg@gmail.com>
2020-12-30 13:01:59 -0300

    Moving AWS provider to the main EKS file.

--- terraform-two/cluster.tf
+++ terraform-two/cluster.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 0.14.0"
 }
+provider "aws" {
+    version = ">= 2.28.1"
+    region = var.region
+    secret_key = var.secret_key
+    access_key = var.access_key
+}
 
 module "eks" {
   source       = "terraform-aws-modules/eks/aws"
--- terraform-two/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-    version = ">= 2.28.1"
-    region = var.region
-    secret_key = var.secret_key
-    access_key = var.access_key
-}
