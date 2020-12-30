1fe8b46c2e9e3a6740837291c1037f9334d23309
marcelo goncalves <tchelojg@gmail.com>
2020-12-30 20:01:29 -0300

    Improving indentation.

--- terraform/aws/security-groups.tf
+++ terraform/aws/security-groups.tf
@@ -1,14 +1,14 @@
 ## Not required. Without it, a default security groups will be used.
 resource "aws_security_group" "security_group" {
-  name_prefix = "sec_group_worker"
-  vpc_id      = module.vpc.vpc_id
+  name_prefix       = "sec_group_worker"
+  vpc_id            = module.vpc.vpc_id
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
 
-    cidr_blocks = [
+    cidr_blocks     = [
       "10.0.0.0/8",
     ]
   }
