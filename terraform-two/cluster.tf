263debfc734f6d7452cec2d34b20c49412b5d8f1
marcelo goncalves <tchelojg@gmail.com>
2020-12-30 12:46:08 -0300

    Cleaning up cluster main config: deleting test deployment.

--- terraform-two/cluster.tf
+++ terraform-two/cluster.tf
@@ -4,95 +4,33 @@ terraform {
 
 module "eks" {
   source       = "terraform-aws-modules/eks/aws"
-  cluster_name    = var.cluster_name
-  cluster_version = "1.17"
+  cluster_name                      = var.cluster_name
+  cluster_version                   = "1.17"
   # Using the private subnets so that the nodes 
-  # are not directly accessible from the internet.
-  subnets         = module.vpc.private_subnets
+  # are not directly accessible from outside.
+  subnets                           = module.vpc.private_subnets
   version = "12.2.0"
-  cluster_create_timeout = "1h"
-  cluster_endpoint_private_access = true 
-  vpc_id = module.vpc.vpc_id
-  wait_for_cluster_cmd = "until curl -k -s $ENDPOINT/healthz >/dev/null; do sleep 4; done"
+  cluster_create_timeout            = "1h"
+  cluster_endpoint_private_access   = true 
+  vpc_id                            = module.vpc.vpc_id
+  wait_for_cluster_cmd              = "until curl -k -s $ENDPOINT/healthz >/dev/null; do sleep 4; done"
   
   worker_groups = [
     {
-      name                          = "worker-group-1"
+      name                          = "worker-group"
       instance_type                 = "t2.micro"
-      asg_desired_capacity          = 2
-      additional_security_group_ids = [aws_security_group.sec_group_worker.id]
+      asg_desired_capacity          = 8
+      additional_security_group_ids = [aws_security_group.security_group.id]
     },
   ]
-
-  worker_additional_security_group_ids = [aws_security_group.all_worker.id]
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.blur_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.blur_cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.blur_cluster.token
-  load_config_file       = false
-  version                = "~> 1.11"
+data "aws_eks_cluster" "blur_cluster" {
+  name                              = module.eks.cluster_id
 }
 
-resource "kubernetes_deployment" "example" {
-  metadata {
-    name = "terraform-example"
-    labels = {
-      test = "MyExampleApp"
-    }
-  }
-
-  spec {
-    replicas = 2
-
-    selector {
-      match_labels = {
-        test = "MyExampleApp"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          test = "MyExampleApp"
-        }
-      }
-
-      spec {
-        container {
-          image = "nginx:1.7.8"
-          name  = "example"
-
-          resources {
-            limits {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-        }
-      }
-    }
-  }
+data "aws_eks_cluster_auth" "blur_cluster" {
+  name                              = module.eks.cluster_id
 }
 
-resource "kubernetes_service" "example" {
-  metadata {
-    name = "terraform-example"
-  }
-  spec {
-    selector = {
-      test = "MyExampleApp"
-    }
-    port {
-      port        = 80
-      target_port = 80
-    }
-
-    type = "LoadBalancer"
-  }
-}
+data "aws_availability_zones" "available" {}
