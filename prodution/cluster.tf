97ccadd87de3db40d5333ae45f35618e41d25fb6
marcelo goncalves <tchelojg@gmail.com>
2020-12-30 13:20:29 -0300

    Indenting file.

--- prodution/cluster.tf
+++ prodution/cluster.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">= 0.14.0"
 }
 provider "aws" {
-    version = ">= 2.28.1"
-    region = var.region
-    secret_key = var.secret_key
-    access_key = var.access_key
+    version                         = ">= 2.28.1"
+    region                          = var.region
+    secret_key                      = var.secret_key
+    access_key                      = var.access_key
 }
 
 module "eks" {
