332f0ae0e557e4d1f17990d238b5ca49892a9289
marcelo goncalves <tchelojg@gmail.com>
2020-12-31 00:10:47 -0300

    Moving s3 ressource to another file remote-state file.

--- terraform/minikube/main.tf
+++ terraform/minikube/main.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-    version                         = ">= 2.28.1"
-    region                          = var.region
-    secret_key                      = var.secret_key
-    access_key                      = var.access_key
-}
-
 provider "helm" {
   kubernetes {
     config_path               = "~/.kube/config-blur-cluster"
@@ -16,7 +9,7 @@ provider "kubernetes" {
 }
 
 module "release-prometheus-operator" {
-  source = "OpenQAI/release-prometheus-operator/helm"
+  source                      = "OpenQAI/release-prometheus-operator/helm"
   helm_chart_namespace        = "default"
   skip_crds                   =  false
   grafana_image_tag           = "7.0.3"
@@ -46,25 +39,4 @@ resource "helm_release" "webservice" {
 resource "helm_release" "worker"{
   name                        = "worker"
   chart                       = "../../k8s/worker"
-}
-
-resource "aws_s3_bucket" "terraform_state_blur_bucket"{
-  bucket = "blur_unique_tf_state_bucket"
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_dynamodb_table"
+} 
